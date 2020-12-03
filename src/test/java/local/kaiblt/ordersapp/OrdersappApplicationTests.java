3f0813d83001c20c756fa6162192f21d43bc842c
kai-blt <blaitong@gmail.com>
2020-12-03 15:00:41 -0800

    initial project setup

---
+++ src/test/java/local/kaiblt/ordersapp/OrdersappApplicationTests.java
@@ -0,0 +1,13 @@
+package local.kaiblt.ordersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
